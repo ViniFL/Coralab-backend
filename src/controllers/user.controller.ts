e063ee5a62af9664b811a1334adce64c440a68ed
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-10-03 23:05:14 -0300

    final commit

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common/exceptions';
+import { UserService } from 'src/services/user.service';
+import { User } from 'src/entities/user.entity';
 
-@Controller('/user')
+@Controller('/users')
 export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Get()
+    getAllUsers(): User[] {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: number): User {
+        const user = this.userService.getUserById(id);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        return user;
+    }
+
+    @Post()
+    createUser(@Body() user: User): User {
+        return this.userService.createUser(user.name, user.email, user.password);
+    }
+
+    @Put(':id')
+    updateUser(
+        @Param('id') id: number,
+        @Body() user: User,
+    ): User {
+        const updatedUser = this.userService.updateUser(id, user.name, user.email, user.password);
+        if (!updatedUser) {
+            throw new NotFoundException('User not found');
+        }
+        return updatedUser;
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: number): void {
+        this.userService.deleteUser(id);
+    }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "src/entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class UserService {
-    private users: User[] = [];
+  private readonly users: User[] = [];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  getUserById(id: number): User | undefined {
+    return this.users.find(user => user.id === id);
+  }
+
+  createUser(name: string, email: string, password: string): User {
+    const newUser = new User(name, email, password);
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateUser(id: number, name: string, email: string, password: string): User | undefined {
+    const userToUpdate = this.users.find(user => user.id === id);
+    if (userToUpdate) {
+      userToUpdate.name = name;
+      userToUpdate.email = email;
+      userToUpdate.password = password;
+      return userToUpdate;
+    }
+    return undefined;
+  }
+
+  deleteUser(id: number): void {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+    }
+  }
 }
